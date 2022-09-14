0a9f5225ba9579645750b05970e335fe87b2be17
Lessalc <lucianoclessa@gmail.com>
2022-09-13 23:45:55 -0300

    Created first Entity and a Resource to test
    - Created the first entity to the project, not implementing any
      annotation to map in the database
    - Created a resource to get a mockup user and return a ResponseEntity
      with this user

---
+++ src/main/java/com/example/workshop/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.example.workshop.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.workshop.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L,"Maria", "maria@email.com", "999999","123456");
+		return ResponseEntity.ok().body(u);
+	}
+}
