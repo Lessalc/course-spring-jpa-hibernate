dc05ff8677c7ca21c95d04a00c85e8250e25e686
Lessalc <lucianoclessa@gmail.com>
2022-09-25 22:43:14 -0300

    Payment entity, association one-to-one

--- src/main/java/com/example/workshop/config/TestConfig.java
+++ src/main/java/com/example/workshop/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.workshop.entities.Category;
 import com.example.workshop.entities.Order;
 import com.example.workshop.entities.OrderItem;
+import com.example.workshop.entities.Payment;
 import com.example.workshop.entities.Product;
 import com.example.workshop.entities.User;
 import com.example.workshop.entities.enums.OrderStatus;
@@ -79,6 +80,9 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 
 	}
 }
