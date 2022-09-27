7d60e68473bb25522477646b795eb59d4b69a00d
Lessalc <lucianoclessa@gmail.com>
2022-09-27 00:44:58 -0300

    Adding methods to insert, delete and update users
    Create the method in the Service class for User
    And the endpoints in the resource class

--- src/main/java/com/example/workshop/services/UserService.java
+++ src/main/java/com/example/workshop/services/UserService.java
@@ -13,15 +13,36 @@ import com.example.workshop.repositories.UserRepository;
 public class UserService {
 
 	@Autowired
-	private UserRepository orderRepository;
+	private UserRepository userRepository;
 	
 	public List<User> findAll(){
-		return orderRepository.findAll();
+		return userRepository.findAll();
 	}
 
 	public User findById(Long id) {
-		Optional<User> userOptinal = orderRepository.findById(id);
+		Optional<User> userOptinal = userRepository.findById(id);
 		
 		return userOptinal.get();
+	}
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getOne(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		// TODO Auto-generated method stub
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
